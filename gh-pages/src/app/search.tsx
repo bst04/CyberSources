cca83b2cf9c62f05216d0c62365d598ec3373241
Ruben Vasquez <ruvasqm@gmail.com>
2025-01-16 15:28:37 -0400

    fix: use gettext in fuzzy search

--- gh-pages/src/app/search.tsx
+++ gh-pages/src/app/search.tsx
@@ -7,17 +7,18 @@ import { data } from './data.json'
 type Source = typeof data extends (infer U)[] ? U : never;
 type FSResult = FuzzyResult<Source>
 const mapResultItem = ({ item, matches: [highlightRanges] }: FSResult): [s:Source,h: HighlightRanges| null] => [item, highlightRanges]
+const getItemText = (item: Source) => [item.name]
 export default function Search() {
   const [queryText, setQueryText] = useState('')
   const [strategy, setStrategy] = useState<FuzzySearchStrategy>('smart')
-  const filtered = useFuzzySearchList({ list: data, queryText, mapResultItem, strategy })
+  const filtered = useFuzzySearchList({ list: data, queryText, mapResultItem, strategy, getText:getItemText })
    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQueryText(event.target.value); // 3. Update the state
   };
   return (
     <>
         <div className="rounded-md p-[3px] bg-gradient-to-r from-banner-start to-banner-end focus-within:motion-safe:animate-bg-loop bg-[length:120%_120%]">
-          <input className="w-80 bg-gray-900 p-2 rounded-md focus:outline-none" placeholder="read input | xargs find -type l " onChange={handleInputChange} />
+          <input className="w-80 bg-gray-900 p-2 rounded-md focus:outline-none" placeholder="" onChange={handleInputChange} />
         </div>
 <ul className="results">
         {filtered.slice(0, 40).map(([item, highlightRanges]) => (
