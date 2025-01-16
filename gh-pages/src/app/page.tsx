6656398049746fd9d7fc78d81dcebf74dbb849be
Ruben Vasquez <ruvasqm@gmail.com>
2025-01-16 13:06:20 -0400

    feat: add logo and input

--- gh-pages/src/app/page.tsx
+++ gh-pages/src/app/page.tsx
@@ -2,12 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen min-w-screen p-8 pb-20 gap-16 sm:px-20 bg-gradient-to-tr from-banner-start to-banner-end  font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col sm:items-start">
-        <div className="bg-black/65 p-4 border-2 w-full rounded-md">
-        <h1 className="text-xl sm:text-3xl lg:text-7xl font-black text-green-500" ><span className="text-white">./</span>cybersources</h1>
-        <h6 className="text-[0.5rem] sm:text-lg font-thin text-gray-50">.a_curated_list_of_cybersecurity_tools_and_resources</h6>
-        {/* <hr className="w-full h-2 my-8 border-0 bg-gradient-to-l from-banner-start to-banner-end motion-safe:animate-bg-loop bg-[length:120%_120%] "/> */}
+    <div className="flex flex-col min-h-screen min-w-screen gap-16  bg-gray-900  font-[family-name:var(--font-geist-sans)]">
+      <div className="flex flex-row gap-8 items-center bg-black/65 w-full p-8">
+        <div className="relative h-16 aspect-square">
+          <Image className="rounded-md left-0" src="/cybersources/logo.svg" fill={true} alt="logo"/>
+        </div>
+        <div className="">
+          <h2 className="text-xl sm:text-3xl lg:text-7xl font-black text-green-500" ><span className="text-white">./</span>cybersources</h2>
+          {/* <hr className="w-full h-2 my-8 border-0 bg-gradient-to-l from-banner-start to-banner-end motion-safe:animate-bg-loop bg-[length:120%_120%] "/> */}
+        </div>
+      </div>
+      <main className="flex flex-col items-center gap-16 px-8  pb-20">
+        <h2 className="text-xl lg:max-w-[50%] sm:text-5xl font-black text-center text-gray-50">A curated list of cybersecurity tools and resources</h2>
+        <div className="rounded-md p-[3px] bg-gradient-to-r from-banner-start to-banner-end focus-within:motion-safe:animate-bg-loop bg-[length:120%_120%]">
+          <input className="w-80 bg-gray-900 p-2 rounded-md focus:outline-none" placeholder="read input | xargs find -t l {} ."/>
         </div>
       </main>
       <footer className="mt-auto row-start-3 flex gap-6 flex-wrap items-center justify-center">
--- gh-pages/tailwind.config.ts
+++ gh-pages/tailwind.config.ts
@@ -11,17 +11,22 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        'banner-start': "#1C00A6",
-        'banner-end': "#F40008"
+        'banner-start': "var(--bstart)",
+        'banner-end': "var(--bend)"
       },
       animation: {
-        'bg-loop': 'bg-loop 4s ease infinite'
+        'bg-loop': 'bg-loop 4s ease infinite',
+        'shadow-loop': 'shadow-loop 4s ease infinite'
       },
       keyframes: {
         'bg-loop': {
-          '0%': { 'background-position-x': '0%', 'background-position-y': '50%' },
-          '50%': { 'background-position-x': '100%', 'background-position-y': '50%' },
-          '100%': { 'background-position-x': '0%', 'background-position-y': '50%' },
+          '0%': { 'background-position-x': '0%', 'background-position-y': '50%',  'box-shadow': "var(--bstart) -2px 5px 15px" },
+          '50%': { 'background-position-x': '100%', 'background-position-y': '50%', 'box-shadow': "var(--bend) -2px 5px 15px" },
+          '100%': { 'background-position-x': '0%', 'background-position-y': '50%', 'box-shadow': "var(--bstart) -2px 5px 15px" },
+        },
+        'shadow-loop': {
+          '0%': { 'shadow': "var(--bstart) 0px 5px 15px"},
+          '100%': { 'shadow': "var(--bend) 0px 5px 15px"},
         }
       }
     },
